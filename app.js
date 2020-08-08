9537a433316b72d8adecafc622dc1adb22df924a
Yazeed AlKhalaf <yazeedfady@gmail.com>
2020-08-08 16:58:49 +0300

    ✒ Set View Engine to ejs

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var indexRouter = require("./routes/index");
 
 var app = express();
 
+app.set("view engine", "ejs");
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
