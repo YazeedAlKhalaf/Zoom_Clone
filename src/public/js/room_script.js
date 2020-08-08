884ad1ccaca4711dc1059cec8341dda674318b99
Yazeed AlKhalaf <yazeedfady@gmail.com>
2020-08-08 22:54:17 +0300

    😎 Make Per-to-Peer video work

--- src/public/js/room_script.js
+++ src/public/js/room_script.js
@@ -1,6 +1,10 @@
+const socket = io();
+
 const videoGrid = document.getElementById("video-grid");
 const myVideo = document.createElement("video");
 myVideo.muted = true;
+
+var peer = new Peer();
 
 navigator.mediaDevices
   .getUserMedia({
@@ -9,7 +13,32 @@ navigator.mediaDevices
   })
   .then((stream) => {
     addVideoStream(myVideo, stream);
+
+    peer.on("call", (call) => {
+      call.answer(stream);
+      const video = document.createElement("video");
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+
+    socket.on("user-connected", (userId) => {
+      connectToNewUser(userId, stream);
+    });
   });
+
+peer.on("open", (id) => {
+  console.log(id);
+  socket.emit("join-room", ROOM_ID, id);
+});
+
+const connectToNewUser = (userId, stream) => {
+  const call = peer.call(userId, stream);
+  const video = document.createElement("video");
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
+};
 
 const addVideoStream = (video, stream) => {
   video.srcObject = stream;
